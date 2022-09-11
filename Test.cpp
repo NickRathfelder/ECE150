3078345401edd9c19ac00870adbfbc52afb5a93d
Nick Rathfelder <90710054+NickyRicky01@users.noreply.github.com>
2022-09-11 14:57:28 -0400

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -5,7 +5,7 @@ int main();
 int main()
 {
     int End {20};
-    for (int start {1}; start >= End; ++start)
+    for (int start {1}; start <= End; ++start)
     {
         std::cout << start << std::endl;
     }
