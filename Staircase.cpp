955ab8f37e7190511d80bb7bd1f617399c058527
Nick Rathfelder <90710054+NickyRicky01@users.noreply.github.com>
2022-09-12 10:11:27 -0400

    Temp 1

---
+++ Staircase.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main();
+
+int main()
+{
+    double x{};
+    std::cout << "Enter value of x: ";
+    std::cin >> x;
+
+    int stair{};
+    if (x >= 0)
+    {
+        for (int i{0}; i <= x; ++i)
+        {
+            if (x-i < 1.0)
+            {
+                stair = i;
+            }
+        }
+    }
+    else
+    {
+        for (int i {0}; i >=x; --i)
+        {
+            if ( x-i > -1.0)
+            {
+                stair = i;
+            }
+        }
+    }
+    std::cout << "The value of stair(x) is: ";
+    std::cout << stair;
+    std::cout << std::endl;
+}
