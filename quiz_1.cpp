ed52a05ef62e06f76ed74be8eb79a2a3c634657c
Nick Rathfelder <90710054+NickyRicky01@users.noreply.github.com>
2022-09-15 19:03:53 -0400

    5

--- quiz_1.cpp
+++ quiz_1.cpp
@@ -13,7 +13,7 @@ int main()
     US = CAD / 1.3;
 
     std::cout << "There are " << US << " USD." << std:: endl;
-    std::cout << "There are " << (US*0.86) << " Pound sterling." << std::endl;
-    std::cout << "There are " << (US*0.87) << " Euro." << std::endl;
-    std::cout << "There are " << (US*100) << " US penny" << std::endl;
+    std::cout << "There are " << ceil(US*0.86) << " Pound sterling." << std::endl;
+    std::cout << "There are " << ceil(US*0.87) << " Euro." << std::endl;
+    std::cout << "There are " << ceil(US*100) << " US penny" << std::endl;
 }
