2db2fb69fa4def75f3ec1d960affd7d44a62437d
NickyRicky01 <topgun0174@gmail.com>
2022-09-15 17:19:13 -0400

    Create quiz_1.cpp

---
+++ quiz_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main();
+
+int main()
+{
+    int CAD {0};
+    std::cout << "Enter a currency in CAD:" << std::endl;
+    std::cin >> CAD;
+
+    double US;
+
+    US = CAD / 1.3;
+
+    std::cout << "There are " << US << " USD." << std:: endl;
+    std::cout << "There are " << (US*0.86) << " Pound sterling." << std::endl;
+    std::cout << "There are " << (US*0.87) << " Euro." << std::endl;
+    std::cout << "There are " << (US*100) << " US penny" << std::endl;
+}
