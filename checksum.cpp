ee5cd79df2ccb796560ae90ddee58f939b16b12b
Nick Rathfelder <90710054+NickyRicky01@users.noreply.github.com>
2022-10-01 23:51:27 -0400

    f
    Checksum v1

--- checksum.cpp
+++ checksum.cpp
@@ -5,14 +5,79 @@
 int main();
 #endif
 
+unsigned int add_checksum( unsigned int n );
+
+unsigned int add_checksum(unsigned int n)
+{
+	int d{0};
+	bool isfinished {0};
+
+	if ((n > 0) && (n < UINT_MAX))
+	{
+		int temp{0};
+		int sum{0};
+
+		for (int i{8}; i > 0; --i)
+		{
+			if (n % (10^i) == 0 )
+			{
+				continue;
+			}
+			else if ((i+1) % 2 == 0 )
+			{
+				temp = n/(10^i) * 2;
+				while(temp > 9)
+				{
+					temp = temp/10 +temp%10;
+				}
+			}
+			else
+			{
+				temp = n/10*i;
+			}
+				sum += temp * 10^i;
+		}
+		d = (sum*9) % 10;
+		n = n * 10 + d;
+
+		return n;
+	} 
+	else
+	{
+		return (UINT_MAX);
+	}
+}
+void add_checksum( unsigned int array[], std::size_t capacity );
+
+void add_checksum( unsigned int array[], std::size_t capacity )
+{
+
+}
+
+bool verify_checksum( unsigned int n );
+
+bool verify_checksum( unsigned int n )
+{
+
+}
+
+unsigned int remove_checksum( unsigned int n );
+
+unsigned int remove_checksum( unsigned int n )
+{
+
+}
+
+void remove_checksum( unsigned int array[], std::size_t capacity );
+
 #ifndef MARMOSET_TESTING
 int main() {
-	unsigned int value_to_protect{21352411};
+	unsigned int value_to_protect{12345678};
 	unsigned int protected_value = add_checksum(value_to_protect);
 	std::cout << "The value " << value_to_protect
 		<< " with the checksum added is " << protected_value
 		<< "." << std::endl;
-	
+	/*
 	if (verify_checksum(protected_value))
 	{
 		std::cout << "The checksum is valid." << std::endl;
@@ -33,6 +98,7 @@ int main() {
 	}
 
     std::cout << std::endl;
+	*/
     return 0;
 }
 #endif
